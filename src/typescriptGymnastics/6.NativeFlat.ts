d7c2a0de008a366ed8a944ed1e2432bc3652f860
张旭 <xu.zhang@fenrir-inc.com.cn>
2022-02-10 14:49:14 +0800

    feat: 5.appendArgument 想函数类型中添加一个参数，参数类型为范型指定 feat: 6.NativeFlat 用于拍平数组 feat: 8.NonEmptyArray 用于限制数组不能为空。

---
+++ src/typescriptGymnastics/6.NativeFlat.ts
@@ -0,0 +1,27 @@
+// *这个还是有局限性，不仅是因为使用了一个中间高级类型 还多使用了一个范型，而且这样的返回只能是一个数组：['a', 'b', 'c', 'd'] 而不是一个联合类型 'a'|'b'|'c'|'d'
+type Loop<T extends any[], Arr extends any[] = []> = T extends [
+  infer Current,
+  ...infer Rest
+]
+  ? Current extends any[]
+    ? Loop<Rest, [...Arr, ...Current]>
+    : Loop<Rest, [...Arr, Current]>
+  : Arr;
+export type NaiveFlat<T extends any[]> = Loop<T>;
+
+// !要返回联合类型，对于对象来说可以使用 {}[keyof] 的方式，虽然对于数组也可以这样使用 但是对于 length 以及原型上的属性和方法都会被遍历到
+// !所以对于数组来说可以直接使用 [][number] !!!   例如 ['a','b','d'][number] = 'a'|'b'|'d'
+export type NaiveFlat2<T extends any[]> = {
+  [Index in keyof T]: T[Index] extends any[] ? T[Index][number] : never;
+}[number];
+// !递归 遍历更深的数组。 假设例子为：[[['a']], ['b', ['c']], 'd', [[[[[[['d']]]], 'e']]]] 在 [Index in keyof T] 时，其实还是遍历四次，但是四次的结果为 ['a','b'|'c', 'd', 'd'|'e'] 所以最后的结果为 'a' | 'b' | 'c' | 'd' | 'e'
+export type DeepFlat<T extends any[]> = {
+  [Index in keyof T]: T[Index] extends any[]
+    ? DeepFlat<T[Index]>
+    : T[Index];
+}[number];
+
+// 测试用例：
+type NaiveResult = NaiveFlat2<[['a'], ['b', 'c'], ['d']]>; // 'a' | 'b' | 'c' | 'd'
+const aaa: NaiveResult = 'a';
+type deepResult = DeepFlat<[[['a']], ['b', ['c']], 'd', [[[[[[['d']]]], 'e']]]]>;
--- src/typescriptGymnastics/ts内置高级类型.ts
+++ src/typescriptGymnastics/ts内置高级类型.ts
@@ -1,21 +1,21 @@
 //* 1. Partial 将类型里的所有属性变成可选的
 type IPartial<T> = {
-  [I in keyof T]?: T[I]
-}
+  [I in keyof T]?: T[I];
+};
 
 //* 2. Required 将类型里的所有属性变成必须的
 type IRequired<T> = {
   [I in keyof T]-?: T[I]; // -? 就是去掉 ?
-}
+};
 
 //* 3.Readonly 将类型的所有属性变成 只读的 也可以使用 -readonly 效果就是 将只读的变成不是只读的
 type IReadonly<T> = {
-  readonly [I in keyof T]: T[I] // *注意这里的 [I in keyof T]: T[I] 并不会将所有属性都变成必须的，这里会保持默认值，可选就是可选不会被修改
-}
+  readonly [I in keyof T]: T[I]; // *注意这里的 [I in keyof T]: T[I] 并不会将所有属性都变成必须的，这里会保持默认值，可选就是可选不会被修改
+};
 
 // *4. Record<K extends keyof any, T> 将 K 中所有属性的值转化为 T 类型
 type IRecord<K extends keyof any, T> = {
-    [P in K]: T;
+  [P in K]: T;
 };
 
 interface PageInfo {
@@ -41,12 +41,10 @@ type IExclude<T, U> = T extends U ? never : T;
 // *这里的 IExclude 其实是这样的 'a' extends 'b'|'c'|'d' ? never : 'a' | 'b' extends 'b'|'c'|'d' ? never : 'b' | 'c' extends 'b'|'c'|'d' ? never : 'c' 所以得出结果是 'a'
 const exclude: IExclude<'a' | 'b' | 'c', 'b' | 'c' | 'd'> = 'a';
 
-
 // *7. Extract<T, U> extract 提取，从 T 中提取 U
 type IExtract<T, U> = T extends U ? T : never;
 
 const extract: IExtract<'a' | 'b' | 'c', 'b' | 'd'> = 'b';
-
 
 // *8. Omit<T, K extends keyof any> Omit 的作用与 Pick 相反，从 T 中去掉 K 类型包含的属性。
 
@@ -56,8 +54,7 @@ const extract: IExtract<'a' | 'b' | 'c', 'b' | 'd'> = 'b';
 // }
 
 // *上面那种删除指定类型的方法不成功，这种就是选出可行的属性
-type IOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>
-
+type IOmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
 
 interface Todo {
   title: string;
@@ -65,17 +62,18 @@ interface Todo {
   completed: boolean;
 }
 
-type TodoPreview = IOmit<Todo, "description">;
+type TodoPreview = IOmit<Todo, 'description'>;
 
 const todo: TodoPreview = {
-  title: "Clean room",
-  completed: false
+  title: 'Clean room',
+  completed: false,
 };
 
 // *9. NonNullable<T> 的作用是过滤掉 T 类型中的 null 以及 undefined 类型
 type INonNullable<T> = T extends null | undefined ? never : T;
 
-const nonNullable: INonNullable<'number' | 'string' | null | {} | undefined> = {};
+const nonNullable: INonNullable<'number' | 'string' | null | {} | undefined> =
+  {};
 
 interface A {
   b: number;
@@ -86,12 +84,26 @@ interface A {
 
 // *这里对 A 接口没有起作用，说明 NonNullable 对对象不起作用
 const aNonNullable: INonNullable<A> = {
-  b: 0, c: "", d: {}, e: null
+  b: 0,
+  c: '',
+  d: {},
+  e: null,
 };
 
 // *10. ReturnType<T> 的作用是 获取 T 函数类型的返回值类型
 // *这里的理解可能有点困难， infer 专门用于声明一个 临时的类型  因为泛型只能使用在 <> 声明好了的类型，但是对于下面的函数,我们并不知道返回值的类型是什么，而且前 <> 并没有进行声明，所以可以使用 infer R 指定一个 临时类型，这个时候 这个 R 就是函数的返回类型。
 // *这里的 T extends (...args: any[]) => any[] 是一个类型限制，限制 T 应当是一个函数。  然后后面大致意思就是  T 如果  extends (...args: any[]) => infer R 这个函数类型， 那么就返回 R 类型， 否则就返回 any 所以就可以函数的返回值类型了
-type IReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer R ? R : any;
+type IReturnType<T extends (...args: any[]) => any> = T extends (
+  ...args: any[]
+) => infer R
+  ? R
+  : never;
 
-const returnType: IReturnType<() => {}> = {};// * 这里的 () => {} 是一个类型 所以 {} 是一个 {} 类型 而不是函数体
+const returnType: IReturnType<() => {}> = {}; // * 这里的 () => {} 是一个类型 所以 {} 是一个 {} 类型 而不是函数体
+
+// *13 Parameters<T> 用于获取 T 函数类型的参数数组类型
+type IParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer R
+) => any
+  ? R
+  : never;
